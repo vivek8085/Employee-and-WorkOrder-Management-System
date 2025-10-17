36c6ee5b6e6f92e6396bb498adfa565ae357d0d7
Vivek Lokolakar <168566636+vivek8085@users.noreply.github.com>
2025-10-17 16:26:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,5 +51,6 @@ app.use("/api/employees", employeeRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
