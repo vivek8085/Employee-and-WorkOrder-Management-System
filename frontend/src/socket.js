86db12bc27be6a536dc49a3ec156b5d31acd67ca
Vivek Lokolakar <168566636+vivek8085@users.noreply.github.com>
2025-10-17 13:40:05 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://employee-and-workorder-management-system.onrender.com", {
   transports: ["websocket"],
 });
+
